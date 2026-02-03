958b36e0dfd4a846e529b5419a05f97c8d42b059
spencer.harston <spencer.harston@ENGR7KCP5S3>
2026-02-03 14:00:06 -0700

    26

---
+++ 2026-02-03_LAB_interface_exercises/Drone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App.Interfaces;
+
+namespace App.Classes
+{
+    internal class Drone : IFlyable, IShootable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Drone flies upward");
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("Drone shoots lasers");
+        }
+    }
+}
--- 2026-02-03_LAB_interface_exercises/Program.cs
+++ 2026-02-03_LAB_interface_exercises/Program.cs
@@ -7,10 +7,13 @@ internal class Program
     {
         Player player = new Player("Hero");
         Enemy enemy = new Enemy("Thrall");
+        Drone drone = new Drone();
 
         player.Move();
         enemy.Move();
         player.Attack();
         enemy.Attack();
+        drone.Fly();
+        drone.Shoot();
     }
 }
